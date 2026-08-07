c6830f7ec1720dc092f3353e4e049bdd8effa902
mukul varshney <mukulvarshney7071@gmail.com>
2026-08-08 01:46:36 +0530

    selenium 1st code

---
+++ src/mukul/App.java
@@ -0,0 +1,47 @@
+package mukul;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+       WebDriver driver = new ChromeDriver();        //type casting
+       driver.get("https://www.shivohamautomation.com/");
+
+       String expectedTitle = "Shivoham Automation Expert - Consultancy Services";
+
+       Thread.sleep(5000);
+       String actualTitle = driver.getTitle();
+       
+       
+       System.out.println(actualTitle);
+
+       if (actualTitle.equals(expectedTitle)) {
+           System.out.println("Test Passed!");
+       } else {
+           System.out.println("Test Failed!");
+       }
+       
+       Thread.sleep(5000);
+       WebElement Element = driver.findElement(By.className("btn-primary"));
+       Element.click();
+
+       Thread.sleep(5000);
+
+       driver.findElement(By.className("navbar-brand")).click();
+
+       Thread.sleep(5000);
+       
+       driver.findElement(By.id("close-popup")).click();
+
+       Thread.sleep(5000);
+
+       driver.quit();   //  close the browser window
+
+     //  driver.close();
+    }
+}
+
+
