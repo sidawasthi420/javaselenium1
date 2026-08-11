8c302adc623c42b650b1ded33854390f6f9a71bb
Tariqahmad8057 <tariqahmad8057@gmail.com>
2026-08-12 00:45:06 +0530

    added xpath class

--- src/tariq/cssSelectors.java
+++ src/tariq/cssSelectors.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class locators {
+public class cssSelectors {
 
     public static void main(String[] args) throws InterruptedException {
 
---
+++ src/tariq/xpaths.java
@@ -0,0 +1,5 @@
+package tariq;
+
+public class xpaths {
+    
+}
