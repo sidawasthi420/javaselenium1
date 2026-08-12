7431d3de307806e80d6c0b0d8d6a8d9282419385
sidawasthi420 <144908193+sidawasthi420@users.noreply.github.com>
2026-08-12 23:04:23 +0530

    Revert "Added css Selector classes"

--- src/selenium/selenium1.java
+++ src/selenium/selenium1.java
@@ -2,11 +2,12 @@ package selenium;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class selenium1 {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         
          WebDriver driver= new ChromeDriver();
          driver.get("https://shivohamautomation.com/");
@@ -16,6 +17,9 @@ public class selenium1 {
          Thread.sleep(5000);
 
          String actualTitle = driver.getTitle();
+
+         //System.out.println(actualTitle);
+         ///System.exit(0);/// it will terminate the programme
 
         if(actualTitle.equals(expectedTitle)){
                System.out.println("Website opened successfully");
@@ -47,19 +51,14 @@ public class selenium1 {
         //Thread.sleep(3000);
 
         //Click on Enhanced Learning
-        //driver.findElement(By.tagName("he5")).click();
+        driver.findElement(By.tagName("he5")).click();
 
-        
-       // Thread.sleep(10000);
-
-        //Course Overview webelement need to get visible on web page
-        //driver.findElement(By.linkText("Enhanced Learning")).click(); //org.openqa.selenium.NoSuchElementException
-
-        //Elevate Your Skills with Tailored, Enhanced Software Training!
-        driver.findElement(By.partialLinkText("Your Skills with Tailored,")).click();
         
         Thread.sleep(10000);
-        
-        driver.quit();
+
+        //Course Overview webelement need to get visible on web page
+        driver.findElement(By.linkText("Course Overview")).isDisplayed();  //org.openqa.selenium.NoSuchElementException
+        // driver.close();// currently opened window
+        driver.quit();/// closes all window
     }
 }
